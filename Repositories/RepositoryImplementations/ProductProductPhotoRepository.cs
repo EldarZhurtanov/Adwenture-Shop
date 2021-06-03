1ae87db0bbdf75a7233e1c82532839041b4200f8
Эльдар Журтанов <pikap.02@mail.ru>
2021-06-03 17:03:53 +0600

    DAL

---
+++ Repositories/RepositoryImplementations/ProductProductPhotoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using Model.Models;
+using Repositories.RepositoryImplementations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.RepositoryInterfaces
+{
+    public class ProductProductPhotoRepository : Repository<ProductProductPhoto>, IProductProductPhotoRepository
+    {
+        public ProductProductPhotoRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
--- Repositories/RepositoryInterfaces/IAddressRepository.cs
+++ Repositories/RepositoryInterfaces/IAddressRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Repositories.RepositoryInterfaces
 {
-    interface IPurchaseOrderHeaderRepository : IRepository<PurchaseOrderHeader>
+    interface IAddressRepository : IRepository<Address>
     {
     }
 }
